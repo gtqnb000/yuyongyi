3146d22cc0da6cb747a77fd8f24783cd63f3cdb1
gtqnb000 <gtqnb000@users.noreply.github.com>
2026-06-09 12:18:44 +0800

    自动更新 06/09 12:18

---
+++ site/downloads/和平精英/光头强升级版2.0/光头强驱动/过电报验证.sh
@@ -0,0 +1,16 @@
+if [ whoami = "root" ]; then
+echo "\n"
+echo "已root执行脚本，运行开始！"
+else
+#非root执行提示：Permission denied，
+#并直接中断命令执行且退出脚本。
+echo ""
+fi
+
+mkdir -p /data/media/0/Android/data/org.telegram.messenger.web/cache
+
+    sleep 1
+touch /data/media/0/Android/data/org.telegram.messenger.web/cache/-6291878504982842949_99.jpg
+
+echo "过验证成功"
+echo
